5f43f1dee6a0f4056bfa6c7a498fb8c60cc00a5e
Roman <telishevskyir@gmail.com>
2025-10-22 17:31:07 +0200

    Backend initial commit

---
+++ src/test/java/com/app/skillswap/SkillswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.skillswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
